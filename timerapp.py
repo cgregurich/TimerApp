3a2c96c5cb97ed45b917f564beb1f2eaf15eadb3
Colin Gregurich <coling112198@gmail.com>
2020-08-11 14:07:13 -0500

    created timerapp; the base of the application

---
+++ timerapp.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+from mainmenu import MainMenu
+from timer import Timer
+from stopwatch import Stopwatch
+from pomodoro import Pomodoro
+
+
+
+
+class TimerApp(tk.Tk):
+	def __init__(self, *args, **kwargs):
+		tk.Tk.__init__(self, *args, **kwargs)
+
+		self.title("Productivity Time")
+
+		container = tk.Frame(self)
+		container.pack(side="top", fill="both", expand=True)
+		container.grid_rowconfigure(0, weight=1)
+		container.grid_columnconfigure(0, weight=1)
+
+		self.frames = {}
+
+		for F in (MainMenu, Timer, Stopwatch, Pomodoro):
+			frame = F(container, self)
+
+			self.frames[F] = frame
+
+			frame.grid(row=0, column=0, sticky="nsew")
+
+		self.show_frame(MainMenu)
+
+	def show_frame(self, cont):
+		frame = self.frames[cont]
+		frame.reset()
+		frame.tkraise()
+
+def main():
+	app = TimerApp()
+	app.mainloop()
+
+if __name__ == '__main__':
+	main()
+
+
+
