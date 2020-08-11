2561f84447eda55d86de3a448a27a7f545bc8582
Jeff Crouse <jcjctech8@gmail.com>
2020-08-10 22:32:13 -0500

    added pomo settings

--- settings.py
+++ settings.py
@@ -8,5 +8,5 @@ TIMER_FONT = ('Consolas', 24, 'bold')
 
 STOPWATCH_FONT = ('Consolas', 24, 'bold')
 
-POMO_WORK_TIME = 25
-POMO_REST_TIME = 5
+POMO_WORK_TIME = 25 * 60 # in seconds
+POMO_BREAK_TIME = 5 * 60 # in seconds
