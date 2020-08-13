8968f0f0071198fb25f98f07ee78ff188bb2cc2b
Colin Gregurich <coling112198@gmail.com>
2020-08-13 15:01:31 -0500

    created module

--- configmanager.py
+++ configmanager.py
@@ -10,6 +10,9 @@ class ConfigManager(configparser.ConfigParser):
 	def check_for_config_file(self):
 		if not self.read(self.FILE_NAME):
 			self.create_config_file()
+		else:
+			self.read(self.FILE_NAME)
+		
 
 
 	def create_config_file(self):
@@ -22,12 +25,15 @@ class ConfigManager(configparser.ConfigParser):
 
 
 	def change_setting(self, key, value):
-		self['SETTINGS'] = {
-			key: value
-		}
+		self.set('SETTINGS', key, value)
 		with open(self.FILE_NAME, 'w') as configfile:
 			self.write(configfile)
 
+
+	def get_setting(self, section, option):
+		with open(self.FILE_NAME, 'r') as configfile:
+			value = self[section][option]
+		return value
 
 
 
