e5c8a921720cf090823f1d8598f81a047b006a38
Colin Gregurich <coling112198@gmail.com>
2020-08-22 21:01:19 -0500

    added autosave setting

--- storedsettings.py
+++ storedsettings.py
@@ -15,4 +15,4 @@ CLOCK_FONT = ('Consolas', 24, 'bold')
 POMO_WORK_TIME = int(mgr.get("SETTINGS", "POMO_WORK_TIME")) # in seconds
 POMO_BREAK_TIME = int(mgr.get("SETTINGS", "POMO_BREAK_TIME")) # in seconds
 
-AUTO_SAVE = mgr.get("SETTINGS", "AUTO_SAVE")
+AUTOSAVE = mgr.get("SETTINGS", "AUTOSAVE")
