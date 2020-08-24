8cf95315ef5eaf00fec6013595907993d80f22a2
Colin Gregurich <coling112198@gmail.com>
2020-08-24 10:50:21 -0500

    added debug and wait values

--- storedsettings.py
+++ storedsettings.py
@@ -16,3 +16,8 @@ POMO_WORK_TIME = int(mgr.get("SETTINGS", "POMO_WORK_TIME")) # in seconds
 POMO_BREAK_TIME = int(mgr.get("SETTINGS", "POMO_BREAK_TIME")) # in seconds
 
 AUTOSAVE = mgr.get("SETTINGS", "AUTOSAVE")
+
+
+debug = mgr.get("SETTINGS", "DEBUG") == "1"
+
+WAIT = 10 if debug else 1000
