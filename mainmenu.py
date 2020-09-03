333ef1f62e148e6f71fc37fbb0694a2e009d5ad4
Colin Gregurich <coling112198@gmail.com>
2020-09-02 19:08:28 -0500

    fixed formatting

--- mainmenu.py
+++ mainmenu.py
@@ -22,29 +22,33 @@ class MainMenu(Frame):
 		self.draw_menu()
 
 	def draw_menu(self):
-		btn_timer = ttk.Button(self.frame_buttons, text="Timer", command=lambda: self.controller.show_frame('Timer'))
+		
+		btn_settings = ttk.Button(self.frame_buttons, text="Settings", command=lambda: self.controller.show_frame('Settings'))
+		btn_tasks = ttk.Button(self.frame_buttons, text="Tasks", command=lambda: self.controller.show_frame("Tasks"))
 		btn_stopwatch = ttk.Button(self.frame_buttons, text="Stopwatch", command=lambda: self.controller.show_frame('Stopwatch'))
 		btn_pomo = ttk.Button(self.frame_buttons, text="Pomodoro", command=lambda: self.controller.show_frame('Pomodoro'))
-		btn_settings = ttk.Button(self.frame_buttons, text="Settings", command=lambda: self.controller.show_frame('Settings'))
-		btn_activities = ttk.Button(self.frame_buttons, text="Tasks", command=lambda: self.controller.show_frame("Tasks"))
-		self.om_current_task = ttk.OptionMenu(self.frame_buttons, self.controller.current_task, "--",  *taskdao.get_all_tasks())
-		lbl_task = Label(self.frame_buttons, text="What are you working on?")
+		btn_timer = ttk.Button(self.frame_buttons, text="Timer", command=lambda: self.controller.show_frame('Timer'))
 		btn_displaydata = ttk.Button(self.frame_buttons, text="Display Data", command=lambda: self.controller.show_frame("DisplayData"))
 		check_debug = Checkbutton(self.frame_buttons, text="DEBUG", variable=self.controller.debug, command=self.check_clicked)
+		lbl_task = Label(self.frame_buttons, text="What are you working on?")
+		self.om_current_task = ttk.OptionMenu(self.frame_buttons, self.controller.current_task, "--",  *taskdao.get_all_tasks())
 
-		
 
 		PADY = 0
-		self.grid_columnconfigure(0, weight=1) # centers buttons in the frame
-		lbl_task.grid(row=0, column=0)
-		self.om_current_task.grid(row=1, column=0)
-		check_debug.grid(row=0, column=0)
-		btn_timer.grid(row=2, column=0, pady=PADY)
-		btn_stopwatch.grid(row=3, column=0, pady=PADY)
-		btn_pomo.grid(row=4, column=0, pady=PADY)
-		btn_settings.grid(row=1, column=0, pady=PADY)
-		btn_activities.grid(row=2, column=0, pady=PADY)
+		btn_settings.grid(row=0, column=0, pady=PADY)
+		btn_tasks.grid(row=1, column=0, pady=PADY)
+		btn_stopwatch.grid(row=2, column=0, pady=PADY)
+		btn_pomo.grid(row=3, column=0, pady=PADY)
+		btn_timer.grid(row=4, column=0, pady=PADY)
 		btn_displaydata.grid(row=5, column=0, pady=PADY)
+		check_debug.grid(row=6, column=0)
+		lbl_task.grid(row=7, column=0)
+		self.om_current_task.grid(row=8, column=1)
+
+
+
+		self.grid_columnconfigure(0, weight=1) # centers buttons in the frame
+		
 		
 	def refresh_option_menu(self):
 		self.om_current_task.destroy()
