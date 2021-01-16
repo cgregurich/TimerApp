54d28bc2c0c3eff49bd2be133e57c1386b5a9f4c
Colin Gregurich <coling112198@gmail.com>
2021-01-16 10:16:17 -0600

    fixed typo

--- session.py
+++ session.py
@@ -81,7 +81,7 @@ class Session():
 
 
 	def get_current_date(self):
-		"""Returns string of current time in format MM-DD-YY"""
+		"""Returns string of current date in format MM-DD-YY"""
 		today = datetime.now()
 		return today.strftime("%m-%d-%y")
 
