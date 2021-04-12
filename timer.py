e1f1186ca24943db06ac603c2fa0b43e169d7a1d
Colin Gregurich <coling112198@gmail.com>
2021-04-12 00:22:49 -0500

    added entry mode and clock functionality

--- timer.py
+++ timer.py
@@ -10,7 +10,7 @@ from booterwidgets import *
 from session import Session
 from sessiondao import SessionDAO
 
-import datetime
+import datetime as dt
 
 sessiondao = SessionDAO()
 
@@ -31,6 +31,7 @@ class Timer(tk.Frame):
 		self.frame_timer_display = tk.Frame(self, bd=3, bg=storedsettings.APP_WIDGET_COLOR)
 
 		self.mode = STOPPED
+		self.entry_mode = TIMER
 
 		self.frame_back_button.grid(row=0, column=0)
 
@@ -58,6 +59,8 @@ class Timer(tk.Frame):
 		# For keeping track of current date at start of session (in case session 
 		# begins at around 00:00 so the date won't be counted as the "next" day)
 		self.start_date = None
+
+		
 
 
 	def back_clicked(self):
@@ -87,6 +90,8 @@ class Timer(tk.Frame):
 
 		self.entries = (self.entry_hours, self.entry_minutes, self.entry_seconds)
 
+		self.mode_btn = BooterButton(self.frame_entries, text=self.entry_mode, font=(storedsettings.FONT, 10), command=self.mode_btn_clicked)
+
 
 		self.btn_control = BooterButton(self.frame_buttons, text='Start', command=self.right_button_clicked, width=6)
 		self.btn_control.bind('enter')
@@ -100,16 +105,29 @@ class Timer(tk.Frame):
 
 		# Put widgets on frame
 		self.lbl_time.grid(row=0, column=0)
-		self.entry_hours.grid(row=1, column=1)
-		colon_lbls[0].grid(row=1, column=2)
-		self.entry_minutes.grid(row=1, column=3)
-		colon_lbls[1].grid(row=1, column=4)
-		self.entry_seconds.grid(row=1, column=5)
-
-		
+		self.entry_hours.grid(row=1, column=0)
+		colon_lbls[0].grid(row=1, column=1)
+		self.entry_minutes.grid(row=1, column=2)
+		colon_lbls[1].grid(row=1, column=3)
+		self.entry_seconds.grid(row=1, column=4)
+		self.mode_btn.grid(row=1, column=5, padx=(15, 0))
 
 		self.btn_cancel.grid(row=2, column=0, padx=(0, 10))
 		self.btn_control.grid(row=2, column=1)
+
+
+	def mode_btn_clicked(self):
+		if self.entry_mode == TIMER:
+			self.entry_mode = CLOCK
+		elif self.entry_mode == CLOCK:
+			self.entry_mode = TIMER
+
+		self.mode_btn.config(text=self.entry_mode)
+
+		# Enables third entry when switched to timer mode, disables it when switched to clock mode
+		self.change_entries_state()
+
+
 
 	def display_task(self):
 		task = self.parent.get_current_task()
@@ -157,17 +175,31 @@ class Timer(tk.Frame):
 
 
 	def change_entries_state(self):
+		"""Disables relevant widgets when clock runs, enables them when clock stops"""
 		if self.mode == STOPPED:
 			for e in self.entries:
 				e.config(state=tk.NORMAL)
+
+			# Also enable the mode button
+			self.mode_btn.config(state=tk.NORMAL)
+
+			# Third entry is disabled when in clock mode
+			if self.entry_mode == CLOCK:
+				self.entry_seconds.config(state=tk.DISABLED)
+
 		else: # RUNNING or PAUSED
 			for e in self.entries:
 					e.config(state=tk.DISABLED)
+			# Also disable the mode button
+			self.mode_btn.config(state=tk.DISABLED)
 
 
 	def start_timer(self):
 		# Used to remember time to log when the timer ends
-		self.original_time = self._get_time_entered_in_seconds()
+		if self.entry_mode == TIMER:
+			self.original_time = self._get_time_entered_in_seconds()
+		elif self.entry_mode == CLOCK:
+			self.original_time = self._calculate_time()
 		seconds = self.original_time
 		if seconds > 0:
 			self.end_type = AUTOMATIC
@@ -176,6 +208,57 @@ class Timer(tk.Frame):
 			self.mode = STOPPED
 		self.start_time = self.get_current_time()
 		self.start_date = self.get_current_date()
+
+
+	def _calculate_time(self):
+		"""When entry_mode is clock, user enters a clock time to run the timer till based off system clock.
+		Need to calculate how many total seconds until that time and return it.
+		Validates for valid clock time, assuming 24 hour clock"""
+		if not self._is_entered_clock_time_valid():
+			return -1 # indicates that timer shouldn't be started
+
+		now = dt.datetime.now()
+		then = self._get_entered_clock_time()
+		delta = then - now
+
+		# Deals with the scenario where the time entered is technically before the current time
+		# Eg. if the time is 23:30 and you entered 00:00 because you want a timer that goes
+		# till midnight, it would think you meant midnight of TODAY which would be 23h 30m in the past
+		# rather than 30 minutes in the future, since "then" is created using today's date
+		if delta.total_seconds() < 0:
+			new_delta = dt.timedelta(days=1)
+			then += new_delta
+			delta = then - now
+
+		return int(delta.total_seconds())
+
+
+	def _get_entered_clock_time(self):
+		"""Gets time entered in entries when entry_mode is on CLOCK. Assumes validation already completed.
+		Returns a dt.time object"""
+		h = int(self.entry_hours.get() or 0)
+		m = int(self.entry_minutes.get() or 0)
+		s = int(self.entry_seconds.get() or 0)
+
+		return dt.datetime.now().replace(hour=h, minute=m, second=s)
+		
+
+
+
+	def _is_entered_clock_time_valid(self):
+		try:
+			# Check that entries either contain valid integers and aren't empty
+			h = int(self.entry_hours.get())
+			m = int(self.entry_minutes.get())
+			# Check for valid clock times (check for negatives and out of range values)
+			if h < 0 or m < 0 or h > 23 or m > 59:
+				raise ValueError
+			
+		except ValueError:
+			messagebox.showerror("Invalid Time", "Time entered must be a valid clock time (24 hour clock format)")
+			return False	
+	
+		return True
 
 
 	def left_button_clicked(self):
@@ -193,7 +276,7 @@ class Timer(tk.Frame):
 	def _get_time_entered_in_seconds(self):
 		"""Helper method that returns total number of time in seconds"""
 		if not self._is_entered_time_valid():
-			return -1
+			return -1 # indicates that timer shouldn't be started
 		h = tk.IntVar()
 		m = tk.IntVar()
 		s = tk.IntVar()
@@ -272,7 +355,7 @@ class Timer(tk.Frame):
 		Why? Depends on if the user wants there to be a popup notification when
 		an untracked session is done
 		"""
-		if storedsettings.UNTRACKED_POPUP == ON:
+		if storedsettings.UNTRACKED_POPUP == ON and self.end_type == AUTOMATIC:
 			messagebox.showinfo("", "Timer is done")
 
 	def tracked_session_done(self):
@@ -294,13 +377,13 @@ class Timer(tk.Frame):
 
 	def get_current_time(self):
 		"""Returns string of current time in format HH:MM"""
-		now = datetime.datetime.now()
+		now = dt.datetime.now()
 		return now.strftime("%H:%M")
 
 
 	def get_current_date(self):
 		"""Returns string of current date in format MM-DD-YY"""
-		today = datetime.datetime.now()
+		today = dt.datetime.now()
 		return today.strftime("%m-%d-%y")
 
 
@@ -340,7 +423,7 @@ class Timer(tk.Frame):
 		hours, seconds = divmod(total_seconds, 3600)
 		minutes, seconds = divmod(seconds, 60)
 
-		time_obj = datetime.time(hours, minutes, seconds)
+		time_obj = dt.time(hours, minutes, seconds)
 		return time_obj
 
 	def get_task_time_as_seconds(self):
@@ -354,7 +437,6 @@ class Timer(tk.Frame):
 
 	def reset(self):
 		self.change_settings()
-		# self.parent.geometry(storedsettings.TIMER_WIN_SIZE)
 		self.display_task()
 
 
